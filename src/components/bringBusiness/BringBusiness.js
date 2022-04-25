c2db5314f7435c91277c9d88e9c20610060a413a
YonJun <jhon_08_2007@hotmail.com>
2022-04-25 10:00:09 -0500

    fix: list bring business

--- src/components/bringBusiness/BringBusiness.js
+++ src/components/bringBusiness/BringBusiness.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from "./bringBusiness.module.css";
 import axios from "axios";
 import { useNavigate } from "react-router";
+import { customAxios } from "../lib/axios";
 
 const BringBusiness = () => {
   const navigate = useNavigate();
@@ -12,13 +13,8 @@ const BringBusiness = () => {
   const [pic, setPic] = useState([]);
   const getPic = async () => {
     try {
-      // const ab = 1;
-      const api = "http://216.238.69.159:3001/get-plantillas";
-
-      var res = await axios.get(api, {
-        headers: { proyecto: "17sd%s#7913G%FJ600" },
-      });
-      // console.log(res);
+      var res = await customAxios.get("/get-plantillas");
+      console.log(res);
       setPic(res.data);
       // console.log("categ", pic[0] ? pic[0].imagen : "Hoho");
     } catch (error) {
@@ -42,53 +38,24 @@ const BringBusiness = () => {
           </div>
           <div className={styles.contentSubHeadingImgs}>
             <div className="row">
-              <div className="col-lg-3 ">
-                <div className={styles.contentSubHeadingImg}>
-                  <img
-                    src={pic[0] ? pic[0].imagen : "loading"}
-                    alt=""
-                    width="100%"
-                    onClick={formNav}
-                  />
-                  <h6 onClick={formNav}>
-                    {pic[0] ? pic[0].titulo : "loading"}
-                  </h6>
-                  <p onClick={formNav}>{pic[0] ? pic[0].nombre : "loading"}</p>
-                </div>
-              </div>
-              <div className="col-lg-3 ">
-                <div className={styles.contentSubHeadingImg}>
-                  <img
-                    src={pic[1] ? pic[1].imagen : "loading"}
-                    alt=""
-                    width="100%"
-                  />
-                  <h6>{pic[1] ? pic[1].titulo : "loading"}</h6>
-                  <p>{pic[1] ? pic[1].nombre : "loading"}</p>
-                </div>
-              </div>
-              <div className="col-lg-3 ">
-                <div className={styles.contentSubHeadingImg}>
-                  <img
-                    src={pic[2] ? pic[2].imagen : "loading"}
-                    alt=""
-                    width="100%"
-                  />
-                  <h6>{pic[2] ? pic[2].titulo : "loading"}</h6>
-                  <p>{pic[2] ? pic[2].nombre : "loading"}</p>
-                </div>
-              </div>
-              <div className="col-lg-3 ">
-                <div className={styles.contentSubHeadingImg}>
-                  <img
-                    src={pic[3] ? pic[3].imagen : "loading"}
-                    alt=""
-                    width="100%"
-                  />
-                  <h6>{pic[3] ? pic[3].titulo : "loading"}</h6>
-                  <p>{pic[3] ? pic[3].nombre : "loading"}</p>
-                </div>
-              </div>
+              {pic && pic.length > 0 ? (
+                pic.map((img) => (
+                  <div className="col-lg-3 " key={img.id}>
+                    <div className={styles.contentSubHeadingImg}>
+                      <img
+                        src={img.imagen}
+                        alt=""
+                        width="100%"
+                        onClick={formNav}
+                      />
+                      <h6 onClick={formNav}>{img.titulo}</h6>
+                      <p onClick={formNav}>{img.nombre}</p>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <div>No records</div>
+              )}
             </div>
           </div>
         </div>
